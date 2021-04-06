2a1680f6bfcb75ef124ab2ed38972881d0d590ea
prakash@Krishan <prakashittech11@gmail.com>
2021-04-06 12:53:18 +0530

    Modified Start class

--- PhoneBookWebApp/src/main/java/com/nit/PhoneBookWebAppApplication.java
+++ PhoneBookWebApp/src/main/java/com/nit/PhoneBookWebAppApplication.java
@@ -2,12 +2,13 @@ package com.nit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class PhoneBookWebAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PhoneBookWebAppApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(PhoneBookWebAppApplication.class, args);
 	}
 
 }
